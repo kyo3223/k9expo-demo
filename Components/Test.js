ddbbe49ef7ae0656246b40c4d198e772d79ad59d
caohl <51940611@qq.com>
2018-03-06 11:10:30 +0800

    demo

---
+++ Components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Test=()=> {
+    return (
+        <Text>
+            test223
+        </Text>
+    )
+}
+
+export default Test
---
+++ My.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { StyleSheet, Text, View } from 'react-native';
+function My() {
+    return (
+        <Text>
+            my
+        </Text>
+    )
+}
+
+export default My
