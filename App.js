6cc0485d2d62583cd5522532cfb82d1eb0ee21ed
caohl <51940611@qq.com>
2018-03-06 16:27:58 +0800

    demo

--- App.js
+++ App.js
@@ -3,16 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import My from './Components/My';
 // import MainJsx from './components/Main/Main';
 import Main from './Components/Main';
+import { NativeRouter, Route, Link } from 'react-router-native'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <My/>
-        <Main/>
-        
-      </View>
+        <NativeRouter>
+          <View style={styles.container}>
+            <Text>test</Text>
+            <Route exact path="/" component={Main} />
+            <Route path="/My" component={My} />
+            <Route path="/topics" component={My} />
+          </View>
+        </NativeRouter>
+      
     );
   }
 }
