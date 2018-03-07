8ee8b861da45ee31db35eed0bf40fa601f98718a
caohl <51940611@qq.com>
2018-03-07 16:10:34 +0800

    demo

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet,Button } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
@@ -13,6 +13,8 @@ export default class LinksScreen extends React.Component {
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
         <ExpoLinksView />
+        <Button onPress={()=>{console.log(this.props);
+          this.props.navigation.navigate('Settings'); }} title="test"/>
       </ScrollView>
     );
   }
